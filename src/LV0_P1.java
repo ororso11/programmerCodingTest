18f095846099b2aa2a0ba784176c2cb2e6fab5b5
ororso11 <ororso135055@gmail.com>
2023-01-18 23:30:06 +0900

    1. 프로그래머스 0단계 몫 구하기

---
+++ src/LV0_P1.java
@@ -0,0 +1,12 @@
+public class LV0_P1 {
+    public static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+    public static void main(String[] args) {
+        // 몫 구하기
+        // 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+        int divison = solution(10,5);
+        System.out.println(divison);
+    }
+}
