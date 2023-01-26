1ce3812c995870e9bbc02ebae063f004bc75fbe6
“ororso11” <ororso135055@gmail.com>
2023-01-26 23:12:37 +0900

    1. 프로그래머스 0단계 두 수의 차

--- src/LV0_P2.java
+++ src/LV0_P2.java
@@ -7,6 +7,5 @@ public class LV0_P2 {
         // 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
 
         int divison = solution(10,5);
-        System.out.println(divison);
     }
 }
